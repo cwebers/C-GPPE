aad20dfc9e4fe762cd2089f019db484328a1d137
Christfried Webers <Christfried.Webers@nicta.com.au>
2012-06-27 12:21:16 +1000

    separate InitMatrix; remove (some) unused variables

--- testcov.cpp
+++ testcov.cpp
@@ -14,6 +14,36 @@
 #include "Covfunc.h"
 #include "Gppe.h"
 
+// Some utility routines
+
+void InitMatrix(MatrixXd & mat)
+{
+    mat(0, 0) =   1.9546;
+    mat(0, 1) =   1.5274;
+    mat(1, 0) =  -0.8292;
+    mat(1, 1) =   0.3836;
+    mat(2, 0) =   0.9937;
+    mat(2, 1) =  -1.5854;
+    mat(3, 0) =  -1.5110;
+    mat(3, 1) =  -1.3003;
+    mat(4, 0) =  -2.3473;
+    mat(4, 1) =   1.9326;
+    mat(5, 0) =   1.2204;
+    mat(5, 1) = - 2.3566;
+    mat(6, 0) =   0.0001;
+    mat(6, 1) =  -0.0505;
+    mat(7, 0) = - 0.1004;
+    mat(7, 1) = - 1.6604;
+    mat(8, 0) =   2.0236;
+    mat(8, 1) =   2.3934;
+    mat(9, 0) =   0.5493;
+    mat(9, 1) =   1.0635;
+    mat(10, 0) =  0.5883;
+    mat(10, 1) =  0.0024;
+    mat(11, 0) =  1.7972;
+    mat(11, 1) = -0.1446;
+}
+
 //Test approc_gppe_laplace_fast functions of the Gppe class need some fixing
 int testapproc_gppe_laplace_fast()
 {
@@ -25,9 +55,9 @@ int testapproc_gppe_laplace_fast()
     TypePair all_pairs(2);
     VectorXd idx_global_1(2), idx_global_2(2), idx_global(4), ind_t(4), ind_x(4);
     MatrixXd pairs(1, 2), t(2, 2), x(2, 3);
-    VectorXd theta_x=VectorXd::Zero(4);
-    VectorXd theta_t=VectorXd::Zero(3);
-    VectorXd f=VectorXd::Zero(6);
+    VectorXd theta_x = VectorXd::Zero(4);
+    VectorXd theta_t = VectorXd::Zero(3);
+    VectorXd f = VectorXd::Zero(6);
     t(0, 0) = -0.7258;
     t(0, 1) = -1.9623;
     t(1, 0) = -0.3078;
@@ -49,14 +79,14 @@ int testapproc_gppe_laplace_fast()
     ind_t << 0, 0, 1, 1;
     ind_x << 0, 1, 0, 1;
 
-    g.Approx_Gppe_Laplace(new CovSEard(),new CovSEard(), theta_x, theta_t, sigma,
-    t, x, all_pairs, idx_global, idx_global_1, idx_global_2, ind_t, ind_x, M, N);
-  // double loglike = g.log_likelihood(f, sigma, all_pairs, idx_global_1, idx_global_2,M, N);
-   			cout<<"W"<<endl<<g.GetW()<<endl;
-			cout<<"L"<<endl<<g.GetL()<<endl;
-			cout<<"Kinv"<<endl<<g.GetKinv()<<endl;
-			cout<<"f"<<endl<<g.Getf()<<endl;
-   return 0;
+    g.Approx_Gppe_Laplace(new CovSEard(), new CovSEard(), theta_x, theta_t, sigma,
+                          t, x, all_pairs, idx_global, idx_global_1, idx_global_2, ind_t, ind_x, M, N);
+    // double loglike = g.log_likelihood(f, sigma, all_pairs, idx_global_1, idx_global_2,M, N);
+    cout << "W" << endl << g.GetW() << endl;
+    cout << "L" << endl << g.GetL() << endl;
+    cout << "Kinv" << endl << g.GetKinv() << endl;
+    cout << "f" << endl << g.Getf() << endl;
+    return 0;
 
 }
 
@@ -118,7 +148,7 @@ int testvoidfunctions()
     t1(1) = 15;
     t2(0) = 1;
     t2(1) = 15;
-   // CovSEard a = CovSEard();
+    // CovSEard a = CovSEard();
     //CovNoise b=CovNoise(t5);
 //CovSum mafunc=CovSum(new CovSEard,new CovSEard,t4);
     //CovSEard mafunc=CovSEard();
@@ -144,30 +174,8 @@ int testCovSEard()
         t4(z) = 1 ;
     }
     t5(0) = 12;
-    mat(0, 0) =   1.9546;
-    mat(0, 1) =   1.5274;
-    mat(1, 0) =  -0.8292;
-    mat(1, 1) =   0.3836;
-    mat(2, 0) =   0.9937;
-    mat(2, 1) =  -1.5854;
-    mat(3, 0) =  -1.5110;
-    mat(3, 1) =  -1.3003;
-    mat(4, 0) =  -2.3473;
-    mat(4, 1) =   1.9326;
-    mat(5, 0) =   1.2204;
-    mat(5, 1) = - 2.3566;
-    mat(6, 0) =   0.0001;
-    mat(6, 1) =  -0.0505;
-    mat(7, 0) = - 0.1004;
-    mat(7, 1) = - 1.6604;
-    mat(8, 0) =   2.0236;
-    mat(8, 1) =   2.3934;
-    mat(9, 0) =   0.5493;
-    mat(9, 1) =   1.0635;
-    mat(10, 0) =  0.5883;
-    mat(10, 1) =  0.0024;
-    mat(11, 0) =  1.7972;
-    mat(11, 1) = -0.1446;
+
+    InitMatrix(mat);
 
     t1(0) = 1;
     t1(1) = 15;
@@ -188,11 +196,6 @@ int testCovSEard()
 }
 
 
-
-
-
-
-
 //Test CovLINard
 int testCovLINard()
 {
@@ -203,30 +206,8 @@ int testCovLINard()
         t4(z) = 1 ;
     }
     t5(0) = 12;
-    mat(0, 0) =   1.9546;
-    mat(0, 1) =   1.5274;
-    mat(1, 0) =  -0.8292;
-    mat(1, 1) =   0.3836;
-    mat(2, 0) =   0.9937;
-    mat(2, 1) =  -1.5854;
-    mat(3, 0) =  -1.5110;
-    mat(3, 1) =  -1.3003;
-    mat(4, 0) =  -2.3473;
-    mat(4, 1) =   1.9326;
-    mat(5, 0) =   1.2204;
-    mat(5, 1) = - 2.3566;
-    mat(6, 0) =   0.0001;
-    mat(6, 1) =  -0.0505;
-    mat(7, 0) = - 0.1004;
-    mat(7, 1) = - 1.6604;
-    mat(8, 0) =   2.0236;
-    mat(8, 1) =   2.3934;
-    mat(9, 0) =   0.5493;
-    mat(9, 1) =   1.0635;
-    mat(10, 0) =  0.5883;
-    mat(10, 1) =  0.0024;
-    mat(11, 0) =  1.7972;
-    mat(11, 1) = -0.1446;
+
+    InitMatrix(mat);
 
     t1(0) = 1;
     t1(1) = 15;
@@ -256,42 +237,18 @@ int testCovLINard()
 
 int testCoSEiso()
 {
-    VectorXd t1(2), t2(2), t3(3), t4(2), t5(1);
-    MatrixXd mat(12, 2);
-    for (int z = 0;z < 2;z++)
-    {
-        t4(z) = 1 ;
-    }
-    t5(0) = 12;
-    mat(0, 0) =   1.9546;
-    mat(0, 1) =   1.5274;
-    mat(1, 0) =  -0.8292;
-    mat(1, 1) =   0.3836;
-    mat(2, 0) =   0.9937;
-    mat(2, 1) =  -1.5854;
-    mat(3, 0) =  -1.5110;
-    mat(3, 1) =  -1.3003;
-    mat(4, 0) =  -2.3473;
-    mat(4, 1) =   1.9326;
-    mat(5, 0) =   1.2204;
-    mat(5, 1) = - 2.3566;
-    mat(6, 0) =   0.0001;
-    mat(6, 1) =  -0.0505;
-    mat(7, 0) = - 0.1004;
-    mat(7, 1) = - 1.6604;
-    mat(8, 0) =   2.0236;
-    mat(8, 1) =   2.3934;
-    mat(9, 0) =   0.5493;
-    mat(9, 1) =   1.0635;
-    mat(10, 0) =  0.5883;
-    mat(10, 1) =  0.0024;
-    mat(11, 0) =  1.7972;
-    mat(11, 1) = -0.1446;
+    VectorXd t1(2);
+    t1 << 1, 15;
 
-    t1(0) = 1;
-    t1(1) = 15;
-    t2(0) = 1;
-    t2(1) = 15;
+    VectorXd t2(2);
+    t2 << 1, 15;
+
+    VectorXd t4(2);
+    t4 << 1, 1;
+
+    MatrixXd mat(12, 2);
+    InitMatrix(mat);
+
     CovSEiso a = CovSEiso(t4);
 
 //cout<<a.Evaluate(t1,t2)<<endl;
@@ -304,42 +261,18 @@ int testCoSEiso()
 
 int testCovNoise()
 {
-    VectorXd t1(2), t2(2), t3(3), t4(1), t5(1);
-    MatrixXd mat(12, 2);
-    for (int z = 0;z < 1;z++)
-    {
-        t4(z) = 1 ;
-    }
-    t5(0) = 12;
-    mat(0, 0) =   1.9546;
-    mat(0, 1) =   1.5274;
-    mat(1, 0) =  -0.8292;
-    mat(1, 1) =   0.3836;
-    mat(2, 0) =   0.9937;
-    mat(2, 1) =  -1.5854;
-    mat(3, 0) =  -1.5110;
-    mat(3, 1) =  -1.3003;
-    mat(4, 0) =  -2.3473;
-    mat(4, 1) =   1.9326;
-    mat(5, 0) =   1.2204;
-    mat(5, 1) = - 2.3566;
-    mat(6, 0) =   0.0001;
-    mat(6, 1) =  -0.0505;
-    mat(7, 0) = - 0.1004;
-    mat(7, 1) = - 1.6604;
-    mat(8, 0) =   2.0236;
-    mat(8, 1) =   2.3934;
-    mat(9, 0) =   0.5493;
-    mat(9, 1) =   1.0635;
-    mat(10, 0) =  0.5883;
-    mat(10, 1) =  0.0024;
-    mat(11, 0) =  1.7972;
-    mat(11, 1) = -0.1446;
+    VectorXd t1(2);
+    t1 << 1, 15;
 
-    t1(0) = 1;
-    t1(1) = 15;
-    t2(0) = 1;
-    t2(1) = 15;
+    VectorXd t2(2);
+    t2 << 1, 15;
+
+    VectorXd t4(1);
+    t4 << 1;
+
+    MatrixXd mat(12, 2);
+    InitMatrix(mat);
+
     CovNoise a = CovNoise(t4);
 
 //cout<<a.Evaluate(t1,t2)<<endl;
@@ -350,51 +283,28 @@ int testCovNoise()
 
 
 
-
 //Test CovSum
 
 int testCovSum()
 {
-    VectorXd t1(2), t2(2), t3(3), t4(4), t5(1);
-    MatrixXd mat(12, 2);
-    for (int z = 0;z < 4;z++)
-    {
-        t4(z) = 1 ;
-    }
-    for (int z = 0;z < 3;z++)
-    {
-        t3(z) = 1 ;
-    }
-    t5(0) = 12;
-    mat(0, 0) =   1.9546;
-    mat(0, 1) =   1.5274;
-    mat(1, 0) =  -0.8292;
-    mat(1, 1) =   0.3836;
-    mat(2, 0) =   0.9937;
-    mat(2, 1) =  -1.5854;
-    mat(3, 0) =  -1.5110;
-    mat(3, 1) =  -1.3003;
-    mat(4, 0) =  -2.3473;
-    mat(4, 1) =   1.9326;
-    mat(5, 0) =   1.2204;
-    mat(5, 1) = - 2.3566;
-    mat(6, 0) =   0.0001;
-    mat(6, 1) =  -0.0505;
-    mat(7, 0) = - 0.1004;
-    mat(7, 1) = - 1.6604;
-    mat(8, 0) =   2.0236;
-    mat(8, 1) =   2.3934;
-    mat(9, 0) =   0.5493;
-    mat(9, 1) =   1.0635;
-    mat(10, 0) =  0.5883;
-    mat(10, 1) =  0.0024;
-    mat(11, 0) =  1.7972;
-    mat(11, 1) = -0.1446;
+    VectorXd t1(2);
+    t1 << 1, 15;
 
-    t1(0) = 1;
-    t1(1) = 15;
-    t2(0) = 1;
-    t2(1) = 15;
+    VectorXd t2(2);
+    t2 << 1, 15;
+
+    VectorXd t5(1);
+    t5(0) = 12;
+
+    VectorXd t3(3);
+    t3 << 1, 1, 1;
+
+    VectorXd t4(4);
+    t4 << 1, 1, 1, 1;
+
+    MatrixXd mat(12, 2);
+    InitMatrix(mat);
+
     CovSum a = CovSum(new CovSEard, new CovNoise, t4);
     CovSEard b = CovSEard(t3);
 
